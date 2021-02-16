45234cd23970a226feb10ddf57859a650697c055
Muhammad Ali <muhammadali.pete@gmail.com>
2021-02-15 23:01:18 -0700

    size of buildings can be adjusted dynamically using input now

--- sunsethills.js
+++ sunsethills.js
@@ -1,8 +1,17 @@
 function firstRectangleColor(){
-    document.getElementById("firstbuilding").style.background = "yellow";
+    document.getElementById("b1").style.background = "yellow";
 
 }
 
 
 firstRectangleColor();
 
+function build_size(idval, size){
+    let target_build = idval.slice(-2,idval.length);   
+    console.log(target_build);
+    console.log(document.getElementById(target_build).style.height);
+    document.getElementById(target_build).style.height = size * 50+ "px";
+    
+    //console.log(size * 500);
+
+}
