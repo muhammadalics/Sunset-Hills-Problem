724b88f7a9bb175dba3a93a4fbe91aaf3bafdce8
Muhammad Ali <muhammadali.pete@gmail.com>
2021-02-18 11:02:06 -0700

    beforeEach and afterEach added

--- sunsethills.test.js
+++ sunsethills.test.js
@@ -4,39 +4,30 @@ const assert = require('assert');
 
 
 describe('Checking proper rendering of different elements', ()=>{
-    var driver;
+    let driver;
 
-    beforeAll(async ()=>{
-        // var driver = await new Builder().forBrowser('chrome').build();
-        // await driver.get('C:/Github Repos/Sunset-Hills-Problem-main/Sunset-Hills-Problem/index.html');
-    
+    beforeEach(async ()=>{
+        driver = await new Builder().forBrowser('chrome').build();
+        await driver.get('C:/Github Repos/Sunset-Hills-Problem-main/Sunset-Hills-Problem/index.html');
     });
-    
+
+    afterEach(async()=>{
+        await driver.quit();
+    });
+
 
     test('Title is correct', async () => {           
-        var driver = await new Builder().forBrowser('chrome').build();
-        await driver.get('C:/Github Repos/Sunset-Hills-Problem-main/Sunset-Hills-Problem/index.html');
         var data = await driver.getTitle();
-        // assert.equal(data, 'Sunset Hills Problem');
         expect(data).toBe('Sunset Hills Problem');
         
       });
 
+    test('Title is correct222222222', async () => {           
+        var data = await driver.getTitle();
+        expect(data).toBe('Sunset Hills Problem');
+        
+      });
+      
 
 });
 
-// beforeEach(async ()=>{
-//     let driver = await new Builder().forBrowser('chrome').build();
-//     return await driver.get('C:/Github Repos/Sunset-Hills-Problem-main/Sunset-Hills-Problem/index.html');
-
-// });
-
-
-// it('Title is correct', async () => {       
-    
-//     // let driver = await new Builder().forBrowser('chrome').build();
-//     // await driver.get('C:/Github Repos/Sunset-Hills-Problem-main/Sunset-Hills-Problem/index.html');
-//     let data = await driver.getTitle();
-//     assert.equal(data, 'Sunset Hills Problem');
-    
-//   });
